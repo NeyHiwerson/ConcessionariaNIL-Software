b500cdd9e81e7a1bfb8992da95764d6c41479399
lorenzo simoes vargas <LuisitoBoludo69@gmail.com>
2024-02-16 22:07:01 -0300

    Foi Criado o formulário Contato do Cliente

---
+++ SoftwareConcessionaria/ContatoCliente.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace SoftwareConcessionaria
+{
+    partial class ContatoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCpfCnpj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtContatoDeLigacao = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtRenda = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dtpCadastro = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(41, 125);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(883, 29);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(579, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ficha De Cadastro Do Cliente";
+            // 
+            // txtCpfCnpj
+            // 
+            this.txtCpfCnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCpfCnpj.Location = new System.Drawing.Point(71, 215);
+            this.txtCpfCnpj.Name = "txtCpfCnpj";
+            this.txtCpfCnpj.Size = new System.Drawing.Size(165, 29);
+            this.txtCpfCnpj.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CPF/CNPJ:";
+            // 
+            // txtRg
+            // 
+            this.txtRg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRg.Location = new System.Drawing.Point(287, 215);
+            this.txtRg.Name = "txtRg";
+            this.txtRg.Size = new System.Drawing.Size(165, 29);
+            this.txtRg.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(282, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "RG:";
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataNascimento.Location = new System.Drawing.Point(511, 215);
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(165, 29);
+            this.txtDataNascimento.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(506, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 58);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Data De Nasc:\r\n\r\n";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(41, 300);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(883, 29);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "E-mail";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(41, 393);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(165, 29);
+            this.txtTelefone.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(36, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 29);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Telefone:";
+            // 
+            // txtContatoDeLigacao
+            // 
+            this.txtContatoDeLigacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContatoDeLigacao.Location = new System.Drawing.Point(43, 483);
+            this.txtContatoDeLigacao.Name = "txtContatoDeLigacao";
+            this.txtContatoDeLigacao.Size = new System.Drawing.Size(254, 29);
+            this.txtContatoDeLigacao.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(40, 451);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(249, 29);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Contato De Ligação:\r\n";
+            // 
+            // txtRenda
+            // 
+            this.txtRenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRenda.Location = new System.Drawing.Point(734, 215);
+            this.txtRenda.Name = "txtRenda";
+            this.txtRenda.Size = new System.Drawing.Size(165, 29);
+            this.txtRenda.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(728, 183);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label9.Size = new System.Drawing.Size(96, 29);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Renda:";
+            // 
+            // dtpCadastro
+            // 
+            this.dtpCadastro.Location = new System.Drawing.Point(43, 544);
+            this.dtpCadastro.Name = "dtpCadastro";
+            this.dtpCadastro.Size = new System.Drawing.Size(200, 20);
+            this.dtpCadastro.TabIndex = 17;
+            this.dtpCadastro.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // ContatoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 619);
+            this.Controls.Add(this.dtpCadastro);
+            this.Controls.Add(this.txtRenda);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtContatoDeLigacao);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCpfCnpj);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "ContatoCliente";
+            this.Text = "ContatoCliente";
+            this.Load += new System.EventHandler(this.ContatoCliente_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCpfCnpj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDataNascimento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtContatoDeLigacao;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtRenda;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dtpCadastro;
+    }
+}
