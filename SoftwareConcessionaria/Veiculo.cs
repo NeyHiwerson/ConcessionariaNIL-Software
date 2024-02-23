ab643850d28d6374c0b5ac1df9a738d56a78d167
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2024-02-22 22:52:24 -0300

    refatoradoVeiculo, falta editar

--- SoftwareConcessionaria/Veiculo.cs
+++ SoftwareConcessionaria/Veiculo.cs
@@ -102,53 +102,18 @@ namespace SoftwareConcessionaria
             }
             else
             {
-                idDoVeiculo = txtVeiIdVeiculo.Text;
-                var urlBase = $"{url}/{idDoVeiculo}";
+                idDoVeiculo = txtVeiIdVeiculo.Text;                
                 try
                 {
-                    using (var cliente = new HttpClient())
+                    VeiculoModel veiculoModel = buscarVeiculoPorId(idDoVeiculo);
+
+                    if (veiculoModel != null)
                     {
-                        var resposta = cliente.GetStringAsync(urlBase);
-                        resposta.Wait();
-
-                        // Deserializa diretamente no objeto veiculoModel
-                        veiculoModel = JsonConvert.DeserializeObject<VeiculoModel>(resposta.Result);
-
-                        txtVeiIdVeiculo.Text = veiculoModel.id_veiculo.ToString();
-                        txtVeiCodRenavan.Text = veiculoModel.codigo_renavam;
-                        txtVeiPlaca.Text = veiculoModel.placa;
-                        txtVeiAnoFabricacao.Text = veiculoModel.ano_fabricacao;
-                        txtVeiAnoModelo.Text = veiculoModel.ano_modelo;
-                        txtVeiAnoExercicioAtual.Text = veiculoModel.exercicio_atual;
-                        txtVeiMarca.Text = veiculoModel.marca;
-                        txtVeiModelo.Text = veiculoModel.modelo;
-                        txtVeiVersao.Text = veiculoModel.versao;
-                        txtVeiEspecie.Text = veiculoModel.especie;
-                        txtVeiTipo.Text = veiculoModel.tipo;
-                        txtVeiChassi.Text = veiculoModel.chassi;
-                        txtVeiCor.Text = veiculoModel.cor;
-                        txtVeiCombustivel.Text = veiculoModel.combustivel;
-                        txtVeiCategoria.Text = veiculoModel.categoria;
-                        txtVeiPotencia.Text = veiculoModel.potencia;
-                        txtVeiMotor.Text = veiculoModel.motor;
-                        txtVeiValvulas.Text = veiculoModel.valvulas;
-                        txtVeiCambio.Text = veiculoModel.cambio;
-                        txtVeiPeso.Text = veiculoModel.peso;
-                        txtVeiEixos.Text = veiculoModel.eixos;
-                        txtVeiCarroceria.Text = veiculoModel.carroceria;
-                        txtVeiLotacao.Text = veiculoModel.lotacao;
-                        txtVeiCapacidade.Text = veiculoModel.capacidade;
-                        txtVeiQuilometragem.Text = veiculoModel.quilometragem;
-                        txtVeiPortas.Text = veiculoModel.portas;
-                        txtVeiCidade.Text = veiculoModel.cidade;
-                        txtVeiEstado.Text = veiculoModel.estado;
-                        txtVeiNome.Text = veiculoModel.nome;
-                        txtVeiCpfCnpj.Text = veiculoModel.cpfcnpj;
-                        txtVeiValor.Text = veiculoModel.valor.ToString("C");
-                        txtVeiDisponivel.Text = stringBoolean(veiculoModel.disponivel);
-                        txtVeiLink1.Text = veiculoModel.link_1;
+                        imprimeVeiculo(veiculoModel);                        
 
                         btnVeiCriar.Enabled = false;
+                        btnVeiAdicionarVenda.Enabled = true;
+                        btnVeiEditar.Enabled = true;
 
                         MessageBox.Show("Dados do veículo carregados com sucesso!");
                     }
@@ -164,42 +129,7 @@ namespace SoftwareConcessionaria
         {
             veiculoModel = new VeiculoModel();
 
-            txtVeiIdVeiculo.Text = "";
-            txtVeiCodRenavan.Text = "";
-            txtVeiPlaca.Text = "";
-            txtVeiAnoFabricacao.Text = "";
-            txtVeiAnoModelo.Text = "";
-            txtVeiAnoExercicioAtual.Text = "";
-            txtVeiMarca.Text = "";
-            txtVeiModelo.Text = "";
-            txtVeiVersao.Text = "";
-            txtVeiEspecie.Text = "";
-            txtVeiTipo.Text = "";
-            txtVeiChassi.Text = "";
-            txtVeiCor.Text = "";
-            txtVeiCombustivel.Text = "";
-            txtVeiCategoria.Text = "";
-            txtVeiPotencia.Text = "";
-            txtVeiMotor.Text = "";
-            txtVeiValvulas.Text = "";
-            txtVeiCambio.Text = "";
-            txtVeiPeso.Text = "";
-            txtVeiEixos.Text = "";
-            txtVeiCarroceria.Text = "";
-            txtVeiLotacao.Text = "";
-            txtVeiCapacidade.Text = "";
-            txtVeiQuilometragem.Text = "";
-            txtVeiPortas.Text = "";
-            txtVeiCidade.Text = "";
-            txtVeiEstado.Text = "";
-            txtVeiNome.Text = "";
-            txtVeiCpfCnpj.Text = "";
-            txtVeiValor.Text = "";
-            txtVeiDisponivel.Text = "";
-
-            btnVeiCriar.Enabled = true;
-            btnVeiEditar.Enabled = false;
-            btnVeiAdicionarVenda.Enabled = false;
+            limparFormulario();
 
             MessageBox.Show("Campos limpos com sucesso!");
 
@@ -238,42 +168,8 @@ namespace SoftwareConcessionaria
             {
                 try
                 {
-                    //codigo para criar veiculo
-                    veiculoModel = new VeiculoModel();
-
-                    veiculoModel.codigo_renavam = txtVeiCodRenavan.Text;
-                    veiculoModel.placa = txtVeiPlaca.Text;
-                    veiculoModel.ano_fabricacao = txtVeiAnoFabricacao.Text;
-                    veiculoModel.ano_modelo = txtVeiAnoModelo.Text;
-                    veiculoModel.exercicio_atual = txtVeiAnoExercicioAtual.Text;
-                    veiculoModel.marca = txtVeiMarca.Text;
-                    veiculoModel.modelo = txtVeiModelo.Text;
-                    veiculoModel.versao = txtVeiVersao.Text;
-                    veiculoModel.especie = txtVeiEspecie.Text;
-                    veiculoModel.tipo = txtVeiTipo.Text;
-                    veiculoModel.chassi = txtVeiChassi.Text;
-                    veiculoModel.cor = txtVeiCor.Text;
-                    veiculoModel.combustivel = txtVeiCombustivel.Text;
-                    veiculoModel.categoria = txtVeiCategoria.Text;
-                    veiculoModel.potencia = txtVeiPotencia.Text;
-                    veiculoModel.motor = txtVeiMotor.Text;
-                    veiculoModel.valvulas = txtVeiValvulas.Text;
-                    veiculoModel.cambio = txtVeiCambio.Text;
-                    veiculoModel.peso = txtVeiPeso.Text;
-                    veiculoModel.eixos = txtVeiEixos.Text;
-                    veiculoModel.carroceria = txtVeiCarroceria.Text;
-                    veiculoModel.lotacao = txtVeiLotacao.Text;
-                    veiculoModel.capacidade = txtVeiCapacidade.Text;
-                    veiculoModel.quilometragem = txtVeiQuilometragem.Text;
-                    veiculoModel.portas = txtVeiPortas.Text;
-                    veiculoModel.cidade = txtVeiCidade.Text;
-                    veiculoModel.estado = txtVeiEstado.Text;
-                    veiculoModel.nome = txtVeiNome.Text;
-                    veiculoModel.cpfcnpj = txtVeiCpfCnpj.Text;
-                    veiculoModel.valor = (decimal)float.Parse(txtVeiValor.Text);
-                    //Implementar as imagens e os links
-                    veiculoModel.link_1 = txtVeiLink1.Text;
-
+                    // Código para criar veículo
+                    VeiculoModel veiculoModel = montarVeiculo();
 
                     using (var cliente = new HttpClient())
                     {
@@ -286,26 +182,30 @@ namespace SoftwareConcessionaria
                         if (resposta.IsSuccessStatusCode)
                         {
                             MessageBox.Show("Veículo criado com sucesso.");
-                            //refatorar e jogar as logicas em funçoes e chamar do botão
-                            //btnVeiLimpar_Click(sender, e);
-                            //txtVeiIdVeiculo.Text = resposta.Id;
-                            //btnVeiBuscarPorId_Click(sender, e);
+
+                            // Aqui você precisa verificar a estrutura da resposta e ajustar conforme necessário
+                            // Supondo que a resposta contenha o VeiculoModel serializado diretamente
+                            var respostaContent = await resposta.Content.ReadAsStringAsync();
+                            veiculoModel = JsonConvert.DeserializeObject<VeiculoModel>(respostaContent);
+
+                            // Refatorar e jogar as lógicas em funções e chamar do botão
+                            limparFormulario();
+                            imprimeVeiculo(veiculoModel);
+
+                            // btnVeiBuscarPorId_Click(sender, e);
                         }
                         else
                         {
                             MessageBox.Show($"Erro ao criar veículo. Código de status: {resposta.StatusCode}");
                         }
                     }
-                    
-
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Erro: {ex.Message}");
                 }
-
             }
-            
+
 
         }
 
@@ -328,5 +228,150 @@ namespace SoftwareConcessionaria
         {
 
         }
+
+        public void limparFormulario()
+        {
+            txtVeiIdVeiculo.Text = "";
+            txtVeiCodRenavan.Text = "";
+            txtVeiPlaca.Text = "";
+            txtVeiAnoFabricacao.Text = "";
+            txtVeiAnoModelo.Text = "";
+            txtVeiAnoExercicioAtual.Text = "";
+            txtVeiMarca.Text = "";
+            txtVeiModelo.Text = "";
+            txtVeiVersao.Text = "";
+            txtVeiEspecie.Text = "";
+            txtVeiTipo.Text = "";
+            txtVeiChassi.Text = "";
+            txtVeiCor.Text = "";
+            txtVeiCombustivel.Text = "";
+            txtVeiCategoria.Text = "";
+            txtVeiPotencia.Text = "";
+            txtVeiMotor.Text = "";
+            txtVeiValvulas.Text = "";
+            txtVeiCambio.Text = "";
+            txtVeiPeso.Text = "";
+            txtVeiEixos.Text = "";
+            txtVeiCarroceria.Text = "";
+            txtVeiLotacao.Text = "";
+            txtVeiCapacidade.Text = "";
+            txtVeiQuilometragem.Text = "";
+            txtVeiPortas.Text = "";
+            txtVeiCidade.Text = "";
+            txtVeiEstado.Text = "";
+            txtVeiNome.Text = "";
+            txtVeiCpfCnpj.Text = "";
+            txtVeiValor.Text = "";
+            txtVeiDisponivel.Text = "";
+            txtVeiLink1.Text = "";
+
+            btnVeiCriar.Enabled = true;
+            btnVeiEditar.Enabled = false;
+            btnVeiAdicionarVenda.Enabled = false;
+        }
+
+        public void imprimeVeiculo(VeiculoModel veiculoModel)
+        {
+            txtVeiIdVeiculo.Text = veiculoModel.id_veiculo.ToString();
+            txtVeiCodRenavan.Text = veiculoModel.codigo_renavam;
+            txtVeiPlaca.Text = veiculoModel.placa;
+            txtVeiAnoFabricacao.Text = veiculoModel.ano_fabricacao;
+            txtVeiAnoModelo.Text = veiculoModel.ano_modelo;
+            txtVeiAnoExercicioAtual.Text = veiculoModel.exercicio_atual;
+            txtVeiMarca.Text = veiculoModel.marca;
+            txtVeiModelo.Text = veiculoModel.modelo;
+            txtVeiVersao.Text = veiculoModel.versao;
+            txtVeiEspecie.Text = veiculoModel.especie;
+            txtVeiTipo.Text = veiculoModel.tipo;
+            txtVeiChassi.Text = veiculoModel.chassi;
+            txtVeiCor.Text = veiculoModel.cor;
+            txtVeiCombustivel.Text = veiculoModel.combustivel;
+            txtVeiCategoria.Text = veiculoModel.categoria;
+            txtVeiPotencia.Text = veiculoModel.potencia;
+            txtVeiMotor.Text = veiculoModel.motor;
+            txtVeiValvulas.Text = veiculoModel.valvulas;
+            txtVeiCambio.Text = veiculoModel.cambio;
+            txtVeiPeso.Text = veiculoModel.peso;
+            txtVeiEixos.Text = veiculoModel.eixos;
+            txtVeiCarroceria.Text = veiculoModel.carroceria;
+            txtVeiLotacao.Text = veiculoModel.lotacao;
+            txtVeiCapacidade.Text = veiculoModel.capacidade;
+            txtVeiQuilometragem.Text = veiculoModel.quilometragem;
+            txtVeiPortas.Text = veiculoModel.portas;
+            txtVeiCidade.Text = veiculoModel.cidade;
+            txtVeiEstado.Text = veiculoModel.estado;
+            txtVeiNome.Text = veiculoModel.nome;
+            txtVeiCpfCnpj.Text = veiculoModel.cpfcnpj;
+            txtVeiValor.Text = veiculoModel.valor.ToString("C");
+            txtVeiDisponivel.Text = stringBoolean(veiculoModel.disponivel);
+            txtVeiLink1.Text = veiculoModel.link_1;
+
+        }
+
+        public VeiculoModel buscarVeiculoPorId(string id)
+        {
+            VeiculoModel veiculoModel = new VeiculoModel();
+            var urlBase = $"{url}/{idDoVeiculo}";
+            try
+            {
+                using (var cliente = new HttpClient())
+                {
+                    var resposta = cliente.GetStringAsync(urlBase);
+                    resposta.Wait();
+
+                    // Deserializa diretamente no objeto veiculoModel
+                    veiculoModel = JsonConvert.DeserializeObject<VeiculoModel>(resposta.Result);
+
+
+                    return veiculoModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: Não achou o ID {ex.Message}");
+                return null;
+            }
+        }
+
+        public VeiculoModel montarVeiculo()
+        {
+            VeiculoModel veiculoModel = new VeiculoModel();
+
+            veiculoModel.codigo_renavam = txtVeiCodRenavan.Text;
+            veiculoModel.placa = txtVeiPlaca.Text;
+            veiculoModel.ano_fabricacao = txtVeiAnoFabricacao.Text;
+            veiculoModel.ano_modelo = txtVeiAnoModelo.Text;
+            veiculoModel.exercicio_atual = txtVeiAnoExercicioAtual.Text;
+            veiculoModel.marca = txtVeiMarca.Text;
+            veiculoModel.modelo = txtVeiModelo.Text;
+            veiculoModel.versao = txtVeiVersao.Text;
+            veiculoModel.especie = txtVeiEspecie.Text;
+            veiculoModel.tipo = txtVeiTipo.Text;
+            veiculoModel.chassi = txtVeiChassi.Text;
+            veiculoModel.cor = txtVeiCor.Text;
+            veiculoModel.combustivel = txtVeiCombustivel.Text;
+            veiculoModel.categoria = txtVeiCategoria.Text;
+            veiculoModel.potencia = txtVeiPotencia.Text;
+            veiculoModel.motor = txtVeiMotor.Text;
+            veiculoModel.valvulas = txtVeiValvulas.Text;
+            veiculoModel.cambio = txtVeiCambio.Text;
+            veiculoModel.peso = txtVeiPeso.Text;
+            veiculoModel.eixos = txtVeiEixos.Text;
+            veiculoModel.carroceria = txtVeiCarroceria.Text;
+            veiculoModel.lotacao = txtVeiLotacao.Text;
+            veiculoModel.capacidade = txtVeiCapacidade.Text;
+            veiculoModel.quilometragem = txtVeiQuilometragem.Text;
+            veiculoModel.portas = txtVeiPortas.Text;
+            veiculoModel.cidade = txtVeiCidade.Text;
+            veiculoModel.estado = txtVeiEstado.Text;
+            veiculoModel.nome = txtVeiNome.Text;
+            veiculoModel.cpfcnpj = txtVeiCpfCnpj.Text;
+            veiculoModel.valor = (decimal)float.Parse(txtVeiValor.Text);
+            //Implementar as imagens e os links
+            veiculoModel.link_1 = txtVeiLink1.Text;
+
+            return veiculoModel;
+        }
+
     }
 }
